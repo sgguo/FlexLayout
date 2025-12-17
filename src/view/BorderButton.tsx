7df46dcf28610f2e68a8a4b302d2b5ec10e209ad
guof <sgguo@hotmail.com>
2025-12-17 18:03:50 +0800

    Add dynamic ID attribute to BorderButton component

--- src/view/BorderButton.tsx
+++ src/view/BorderButton.tsx
@@ -180,8 +180,11 @@ export const BorderButton = (props: IBorderButtonProps) => {
         );
     }
 
+    const providedId = node.getAttr("id") as string | undefined;
+
     return (
         <div
+            id={providedId ? `${providedId}-tab` : undefined}
             ref={selfRef}
             data-layout-path={path}
             className={classNames}
