96130e491d62cfd01442044c20515d6619cc9f8c
sgguo <sgguo@hotmail.com>
2025-12-20 22:54:35 +0800

    Fix i18n issue.

--- src/I18nLabel.ts
+++ src/I18nLabel.ts
@@ -8,6 +8,6 @@ export enum I18nLabel {
     Restore = "Restore tab set",
     Popout_Tab = "Popout selected tab",
     Overflow_Menu_Tooltip = "Hidden tabs",
-    Error_rendering_component = "Error rendering component",
+    Error_rendering_component = "Error rendering widget",
     Error_rendering_component_retry = "Retry",
 }
